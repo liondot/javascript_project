0d660f8e86f695db18e3cc85c9b084aef4b12b7c
liondot <51257576+liondot@users.noreply.github.com>
2022-06-25 17:16:31 +0900

    사용자와 컴퓨터의 슛 성공과 실패에 따른 점수판 변경

---
+++ 농구게임/main.js
@@ -0,0 +1,80 @@
+// 게임로직 
+// 1. 컴퓨터부터 시작
+// 2. 사용자가 컴퓨터의 슛 버튼을 클릭 
+// 3. 컴퓨터는 2점슛을 쏠 지, 3점 슛을 쏠 지 랜덤으로 결정 
+// 4. 슛이 성공하면 컴퓨터의 점수가 올라간다.
+// 5. 사용자가 슛을 넣을 차례
+// 6. 사용자가 2점 슛 또는 3점슛 버튼을 클릭 
+// 6-1 2점슛의 성공률은 50%, 3점슛의 성공률은 33%
+// 7. 슛이 성공하면 사용자의 점수를 올려준다.
+// 8. 컴퓨터의 차례로 바뀌고 남은 슛 횟수를 1만큼 줄인다.
+// 9. 남은 슛 횟수가 0이 될때 까지 1~8번 항목을 반복
+// 10. 양쪽의 점수를 비교해 승자를 정한다
+
+let comScore = 0;
+let userScore = 0;
+let isComputerTurn = true;
+
+function onComputerShoot() {
+  if(!isComputerTurn) {
+    return;
+  }
+
+  let shootType = Math.random() < 0.5 ? 2 : 3;
+  let textElem = document.getElementById('text');
+
+
+  if (shootType === 2) {
+    if (Math.random() < 0.5) {
+      textElem.innerHTML = '컴퓨터가 2점슛을 성공시켰습니다!'
+      comScore += 2;
+      let comScoreElem = document.getElementById('computer-score');
+      comScoreElem.innerHTML = comScore;
+
+    } else {
+      textElem.innerHTML = '컴퓨터가 2점슛을 실패했습니다.'
+    }
+  } else {
+    if (Math.random() < 0.33) {
+      textElem.innerHTML = '컴퓨터가 3점슛을 성공시켰습니다!'
+      comScore += 3;
+      let comScoreElem = document.getElementById('computer-score');
+      comScoreElem.innerHTML = comScore;
+    } else {
+      textElem.innerHTML = '컴퓨터가 3점슛을 실패했습니다!'
+    }
+  }
+
+  isComputerTurn = false;
+
+}
+
+function onUserShoot(shootType) {
+
+  if(isComputerTurn) {
+    return;
+  }
+
+  let textElem = document.getElementById('text');
+  let userScoreElem = document.getElementById('user-score');
+
+  if(shootType === 2) {
+    if(Math.random() < 0.5) {
+      textElem.innerHTML = '사용자가 2점슛을 성공했습니다.'
+      userScore += 2;
+      userScoreElem.innerHTML = userScore;
+    } else {
+      textElem.innerHTML = '사용자가 2점슛을 실패했습니다.'
+    }
+  } else {
+    if(Math.random() < 0.33) {
+      textElem.innerHTML = '사용자가 3점슛을 성공했습니다.'
+      userScore += 3
+      userScoreElem.innerHTML = userScore 
+    } else {
+      textElem.innerHTML = '사용자가 3점슛을 실패했습니다.'
+    }
+  }
+
+  isComputerTurn = true;
+}
