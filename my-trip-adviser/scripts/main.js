b40f14dcfde148b68b5aca11cd8c0217c9b6b941
liondot <51257576+liondot@users.noreply.github.com>
2022-07-07 17:24:44 +0900

    my trip adviser 배경, 해더 만들기

---
+++ my-trip-adviser/scripts/main.js
@@ -0,0 +1,11 @@
+$(function(){
+  $(window).scroll(function () {
+    let top = $(window).scrollTop();
+
+    if(top > 0){
+      $('#header').addClass('inverted');
+    } else {
+      $('#header').removeClass('inverted');
+    }
+  })
+})
--- 패스트푸드점 찾기/js/main.js
+++ 패스트푸드점 찾기/js/main.js
@@ -6,7 +6,7 @@ $(function () {
   $('.btn-search').click(function () {
     let searchKeyword = $('#txt-search').val();
 
-    search(searchKeyword);
+    search(1, 10, searchKeyword);
   });
 
   $('#txt-search').on('keypress', function(e) {
@@ -17,9 +17,16 @@ $(function () {
 });
 
 
-function search(searchKeyword) {
+function search(page, perPage, total) {
+  if(typeof page !== 'number' || page < 1)
+  page = 1;
+
+  if(typeof perPage !== 'number' || perPage <= 0)
+  perPage = 10;
 
 $.get(API_URL, {
+  page : page,
+  perPage : perPage,
   searchKeyword: searchKeyword
 }, function (data) {
   let list = data.list;
@@ -47,5 +54,45 @@ $.get(API_URL, {
     $list.append($elem)
   }
 
+  showPaging(page);
+
 });
+}
+
+function showPaging(page, perPage, total) {
+  let $paging = $('.paging').empty();
+  
+
+  let numPages = 5;
+  let pageStart = Math.floor((page - 1) / numPages) * numPages + 1;
+  let pageEnd = pageStart + numPages - 1; 
+  let totalPages = Math.floor((total - 1) / perPage) + 1;
+
+  if(pageEnd >  totalPages)
+  pageEnd = totalPages
+
+  let prevPage = pageStart - 1;
+  if(prevPage < 1);
+  prevPage = 1;
+ 
+  let nextPage = pageEnd + 1; 
+  if(nextPage > totalPages) 
+  nextPage = totalPages;
+
+  let $prevElem = $('<a href = "javascript:search(' + prevPage + ',' + perPage + ',\'' + searchKeyword + '\')">이전</a>')
+  $prevElem.addClass('prev');
+  $paging.append($prevElem);
+
+  for(let i = pageStart; i<= pageEnd; i++){
+    let $elem = $('<a href = "javascript:search(' + i + ',' + perPage + ')">' + i + '</a>');
+
+    if(i === page){
+      $elem.addClass('current');
+    }
+    $paging.append($elem);
+  }
+
+  let $nextElem = $('<a href = "javascript:search(' + nextPage + ',' + perPage + ')">다음</a>')
+  $prevElem.addClass('next');
+  $paging.append($nextElem);
 }
