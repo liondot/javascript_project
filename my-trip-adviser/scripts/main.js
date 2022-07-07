0254420c772520fe95233077ae04c134006bb7b9
liondot <51257576+liondot@users.noreply.github.com>
2022-07-07 18:33:28 +0900

    시작 일정 datapicker 컴포넌트 구현

--- my-trip-adviser/scripts/main.js
+++ my-trip-adviser/scripts/main.js
@@ -9,3 +9,12 @@ $(function(){
     }
   })
 })
+
+$(window).trigger('scroll');
+// $('#from').datepicker();
+let dpFrom = $('#from').datepicker({
+  dateFormat: 'yy-mm-dd',
+  minDate:0
+});
+dpFrom.datepicker('setDate', new Date());
+
